544856c097a7d349c241f274f8b24a3b046b0fab
Shfldeveloper <125667442+Shfldeveloper@users.noreply.github.com>
2024-08-13 22:56:40 +0330

    adding deleting comments by admin api

--- controllers/v1/comment.js
+++ controllers/v1/comment.js
@@ -1,12 +1,22 @@
 const commentModel = require('../../models/comment')
 const movieModel = require('../../models/movie')
-
+const mongoose = require('mongoose')
 exports.addComment = async (req, res) => {
     const { body, movieHref, score } = req.body
 
     const movie = await movieModel.findOne({ href: movieHref }).lean()
 
-    const comment = await commentModel.create({ body, score, movie: movie._id, creator: req.user._id, isAnswer : 0 , isAccept : 0})
+    const comment = await commentModel.create({ body, score, movie: movie._id, creator: req.user._id, isAnswer: 0, isAccept: 0 })
 
     res.status(201).json(comment)
+}
+
+exports.remove = async (req, res) => {
+    const commentID = req.params.id
+    const isIdValid = mongoose.Types.ObjectId.isValid(commentID)
+    if(!isIdValid){
+        return res.status(402).json({message : 'id is not valid'})
+    }
+    const deletedComment = await commentModel.deleteOne({_id : commentID})
+    return res.json(deletedComment)
 }
--- routes/v1/comment.js
+++ routes/v1/comment.js
@@ -6,5 +6,6 @@ const isAdminMiddleware = require('../../middlewares/isAdmin')
 const router = express.Router()
 
 router.route("/").post(authMiddleware,commentController.addComment)
+router.route("/:id").delete(authMiddleware,isAdminMiddleware,commentController.remove)
 
 module.exports = router
